b131ec3d770cacbaf5ba720bf81c35052168f8b9
CrossVas <cross.wanted.1.4@gmail.com>
2024-12-08 14:26:46 +0900

    Initial commit

---
+++ src/main/java/dev/vintage/cropnalyzer/AdvancedCropnalyzer.java
@@ -0,0 +1,107 @@
+package dev.vintage.cropnalyzer;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.relauncher.FMLInjectionData;
+import dev.vintage.cropnalyzer.item.ItemAdvancedAnalyzer;
+import ic2.api.item.ElectricItem;
+import ic2.api.item.IElectricItem;
+import ic2.api.item.Items;
+import ic2.core.util.StackUtil;
+import mods.vintage.core.helpers.ConfigHelper;
+import mods.vintage.core.platform.lang.ILangProvider;
+import mods.vintage.core.platform.lang.LangManager;
+import net.minecraft.client.Minecraft;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.StatCollector;
+import net.minecraftforge.common.Configuration;
+import net.minecraftforge.oredict.ShapedOreRecipe;
+import org.lwjgl.input.Keyboard;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+@Mod(modid = AdvancedCropnalyzer.ID, name = AdvancedCropnalyzer.NAME, version = AdvancedCropnalyzer.VERSION, acceptedMinecraftVersions = AdvancedCropnalyzer.MINECRAFT, dependencies = AdvancedCropnalyzer.DEPS)
+public class AdvancedCropnalyzer implements ILangProvider {
+
+    public static final String ID = "advancedcropnalyzer";
+    public static final String NAME = "IC2: Advanced Cropnalyzer";
+    public static final String VERSION = "1.5.2-1.0.0";
+    public static final String MINECRAFT = "[1.5.2]";
+    public static final String DEPS = "required-after:VintageCore;required-after:IC2";
+
+    public static final String SNEAK_KEY = StatCollector.translateToLocal("key.sneak");
+
+    public static Configuration CONFIG;
+    public static int CROPNALYZER_ID = 2001;
+    public static String[] LABGS;
+
+    public static Item ADVANCED_CROPNALYZER;
+
+    public AdvancedCropnalyzer() {}
+
+    @Mod.PreInit
+    public void preInit(FMLPreInitializationEvent e) {
+        initConfig();
+        LangManager.THIS.registerLangProvider(this);
+        ADVANCED_CROPNALYZER = new ItemAdvancedAnalyzer(CROPNALYZER_ID);
+    }
+
+    @Mod.Init
+    public void init(FMLInitializationEvent e) {
+        GameRegistry.addRecipe(new ShapedOreRecipe(ADVANCED_CROPNALYZER,
+                " D ", "RCR", "LRL",
+                'D', Item.diamond,
+                'R', Item.redstone,
+                'L', new ItemStack(Item.dyePowder, 1, 4),
+                'C', Items.getItem("cropnalyzer")));
+    }
+
+    public void initConfig() {
+        CONFIG = new Configuration(new File((File) FMLInjectionData.data()[6], "config/ic2/advancedcropnalyzer.cfg"));
+        CONFIG.load();
+        CROPNALYZER_ID = ConfigHelper.getId(CONFIG, "IDs", "advanced_cropnalyzer", CROPNALYZER_ID);
+        LABGS = ConfigHelper.getStrings(CONFIG, "localizations", "localizations", new String[] { "en_US" }, "Supported localizations. Place your <name>.lang file in config/advancedcropnalyzer/lang folder or inside mods/advancedcropnalyzer/lang inside modJar");
+        if (CONFIG.hasChanged()) CONFIG.save();
+    }
+
+    @Override
+    public String getModid() {
+        return ID;
+    }
+
+    @Override
+    public List<String> getLocalizationList() {
+        return Arrays.asList(LABGS);
+    }
+
+    public static ItemStack getCharged(Item item, int charge) {
+        if (!(item instanceof IElectricItem)) {
+            throw new IllegalArgumentException(item + " must be an instanceof IElectricItem");
+        } else {
+            ItemStack ret = new ItemStack(item);
+            ElectricItem.manager.charge(ret, charge, Integer.MAX_VALUE, true, false);
+            return ret;
+        }
+    }
+
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    public static void addChargeVariants(Item item, List list) {
+        list.add(getCharged(item, Integer.MAX_VALUE));
+        list.add(getCharged(item, 0));
+    }
+
+    public static int getCharge(ItemStack stack) {
+        NBTTagCompound tag = StackUtil.getOrCreateNbtData(stack);
+        return tag.getInteger("charge");
+    }
+
+    public static boolean isShiftKeyDown() {
+        return Keyboard.isKeyDown(Minecraft.getMinecraft().gameSettings.keyBindSneak.keyCode);
+    }
+}
---
+++ src/main/java/dev/vintage/cropnalyzer/item/ItemAdvancedAnalyzer.java
@@ -0,0 +1,212 @@
+package dev.vintage.cropnalyzer.item;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import dev.vintage.cropnalyzer.AdvancedCropnalyzer;
+import ic2.api.crops.CropCard;
+import ic2.api.item.ElectricItem;
+import ic2.api.item.IElectricItem;
+import ic2.core.IC2;
+import ic2.core.IHasGui;
+import ic2.core.block.TileEntityCrop;
+import ic2.core.block.crop.IC2Crops;
+import ic2.core.item.IHandHeldInventory;
+import ic2.core.item.tool.ContainerCropnalyzer;
+import ic2.core.item.tool.HandHeldCropnalyzer;
+import ic2.core.util.StackUtil;
+import mods.vintage.core.platform.lang.FormattedTranslator;
+import net.minecraft.block.Block;
+import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.EnumToolMaterial;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemTool;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
+
+import java.util.List;
+
+public class ItemAdvancedAnalyzer extends ItemTool implements IHandHeldInventory, IElectricItem {
+
+    public ItemAdvancedAnalyzer(int id) {
+        super(id, 0, EnumToolMaterial.WOOD, new Block[0]);
+        this.setMaxStackSize(1);
+        this.setMaxDamage(27);
+        this.setNoRepair();
+        this.setUnlocalizedName("advanced.cropnalyzer");
+        this.setCreativeTab(IC2.tabIC2);
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void getSubItems(int is, CreativeTabs tabs, List items) {
+        AdvancedCropnalyzer.addChargeVariants(this, items);
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void registerIcons(IconRegister icons) {
+        this.itemIcon = icons.registerIcon(AdvancedCropnalyzer.ID + ":advanced_cropnalyzer");
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public String getItemDisplayName(ItemStack stack) {
+        return FormattedTranslator.GREEN.literal(super.getItemDisplayName(stack));
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void addInformation(ItemStack stack, EntityPlayer player, List tooltip, boolean isDebugMode) {
+        tooltip.add(FormattedTranslator.AQUA.format("message.info.energy", AdvancedCropnalyzer.getCharge(stack), this.getMaxCharge(stack), FormattedTranslator.WHITE.format("message.info.energy.tier", FormattedTranslator.YELLOW.literal(this.getTier(stack) + ""))));
+        if (AdvancedCropnalyzer.isShiftKeyDown()) {
+            tooltip.add(FormattedTranslator.GRAY.format("message.info.click.block", FormattedTranslator.GOLD.literal(AdvancedCropnalyzer.SNEAK_KEY), FormattedTranslator.GOLD.literal("Right Click"), FormattedTranslator.GREEN.format("message.info.crop.info")));
+        } else {
+            tooltip.add(FormattedTranslator.GRAY.format("message.info.press", FormattedTranslator.GREEN.format(AdvancedCropnalyzer.SNEAK_KEY)));
+        }
+    }
+
+    @Override
+    public boolean onItemUseFirst(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ) {
+        if (IC2.platform.isSimulating()) {
+            if (player.isSneaking()) {
+                TileEntity clickedTile = world.getBlockTileEntity(x, y, z);
+                if (clickedTile instanceof TileEntityCrop) {
+                    TileEntityCrop cropTile = (TileEntityCrop) clickedTile;
+                    int scanLevel = cropTile.getScanLevel();
+                    // stats info
+                    int growth = cropTile.getGrowth();
+                    int gain = cropTile.getGain();
+                    int resistance = cropTile.getResistance();
+                    // storage info
+                    int fertilizer = cropTile.getNutrientStorage();
+                    int water = cropTile.getHydrationStorage();
+                    int weedex = cropTile.getWeedExStorage();
+                    // environment info
+                    int nutrients = cropTile.getNutrients();
+                    int humidity = cropTile.getHumidity();
+                    int env = cropTile.getAirQuality();
+                    int light = cropTile.getLightLevel();
+                    if (cropTile.getID() != -1) {
+                        CropCard crop = IC2Crops.instance.getCropList()[cropTile.getID()];
+                        if (crop != null) {
+                            if (scanLevel < 4 && ElectricItem.manager.canUse(stack, 500)) {
+                                cropTile.setScanLevel((byte) ++scanLevel);
+                                IC2.platform.messagePlayer(player, FormattedTranslator.GREEN.format("crop.scan.level.set", FormattedTranslator.AQUA.literal("" + scanLevel)));
+                                ElectricItem.manager.use(stack, 500, player);
+                            }
+                            if (scanLevel >= 4) {
+                                IC2.platform.messagePlayer(player, "=======================");
+                                IC2.platform.messagePlayer(player, FormattedTranslator.WHITE.format("crop.name", FormattedTranslator.GREEN.literal(crop.name())));
+                            }
+
+                            if (scanLevel < 1 && !crop.isWeed(cropTile)) {
+                                IC2.platform.messagePlayer(player, FormattedTranslator.WHITE.format("crop.discovered.by", FormattedTranslator.AQUA.format("crop.discovered.by.unknown")));
+                            } else if (scanLevel >= 4) {
+                                IC2.platform.messagePlayer(player, FormattedTranslator.WHITE.format("crop.discovered.by", FormattedTranslator.AQUA.literal(crop.discoveredBy())));
+                                IC2.platform.messagePlayer(player, FormattedTranslator.YELLOW.format("crop.stats"));
+                                IC2.platform.messagePlayer(player, FormattedTranslator.DARK_GREEN.format("crop.stats.growth", growth, 31));
+                                IC2.platform.messagePlayer(player, FormattedTranslator.GOLD.format("crop.stats.gain", gain, 31));
+                                IC2.platform.messagePlayer(player, FormattedTranslator.DARK_AQUA.format("crop.stats.resistance", resistance, 31));
+
+                                int stress = (crop.tier() - 1) * 4 + growth + gain + resistance;
+                                int maxStress = crop.weightInfluences(cropTile, humidity, nutrients, env) * 5;
+                                IC2.platform.messagePlayer(player, FormattedTranslator.AQUA.format("crop.stats.needs", stress, maxStress));
+                            }
+
+
+                        }
+                        if (scanLevel >= 4) {
+                            IC2.platform.messagePlayer(player, FormattedTranslator.YELLOW.format("crop.storage"));
+                            IC2.platform.messagePlayer(player, FormattedTranslator.GOLD.format("crop.storage.fertilizer", fertilizer, 100));
+                            IC2.platform.messagePlayer(player, FormattedTranslator.BLUE.format("crop.storage.water", water, 200));
+                            IC2.platform.messagePlayer(player, FormattedTranslator.LIGHT_PURPLE.format("crop.storage.weedex", weedex, 150));
+
+                            IC2.platform.messagePlayer(player, FormattedTranslator.YELLOW.format("crop.environment"));
+                            IC2.platform.messagePlayer(player, FormattedTranslator.GREEN.format("crop.env.nutrients", nutrients, 20));
+                            IC2.platform.messagePlayer(player, FormattedTranslator.DARK_AQUA.format("crop.env.humidity", humidity, 20));
+                            IC2.platform.messagePlayer(player, FormattedTranslator.AQUA.format("crop.env.env", env, 10));
+                            IC2.platform.messagePlayer(player, FormattedTranslator.YELLOW.format("crop.env.light", light, 15));
+                            IC2.platform.messagePlayer(player, "=======================");
+                        }
+                    } else {
+                        IC2.platform.messagePlayer(player, FormattedTranslator.WHITE.format("crop.name", FormattedTranslator.GREEN.format("crop.discovered.by.unknown")));
+                    }
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player) {
+        if (IC2.platform.isSimulating()) {
+            if (!player.isSneaking()) {
+                IC2.platform.launchGui(player, this.getInventory(player, stack));
+            }
+        }
+        return stack;
+    }
+
+    @Override
+    public IHasGui getInventory(EntityPlayer player, ItemStack stack) {
+        return new HandHeldCropnalyzer(player, stack);
+    }
+
+    @Override
+    public boolean onDroppedByPlayer(ItemStack item, EntityPlayer player) {
+        if (item != null && player.openContainer instanceof ContainerCropnalyzer) {
+            HandHeldCropnalyzer cropnalyzer = ((ContainerCropnalyzer)player.openContainer).cropnalyzer;
+            NBTTagCompound tag = StackUtil.getOrCreateNbtData(item);
+            if (cropnalyzer.matchesUid(tag.getInteger("uid"))) {
+                player.closeScreen();
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onBlockDestroyed(ItemStack stack, World world, int blockID, int x, int y, int z, EntityLiving user) {
+        return false;
+    }
+
+    @Override
+    public boolean hitEntity(ItemStack stack, EntityLiving attacker, EntityLiving target) {
+        return false;
+    }
+
+    @Override
+    public boolean canProvideEnergy(ItemStack stack) {
+        return false;
+    }
+
+    @Override
+    public int getChargedItemId(ItemStack stack) {
+        return this.itemID;
+    }
+
+    @Override
+    public int getEmptyItemId(ItemStack stack) {
+        return this.itemID;
+    }
+
+    @Override
+    public int getMaxCharge(ItemStack stack) {
+        return 10000;
+    }
+
+    @Override
+    public int getTier(ItemStack stack) {
+        return 1;
+    }
+
+    @Override
+    public int getTransferLimit(ItemStack stack) {
+        return 500;
+    }
+}
